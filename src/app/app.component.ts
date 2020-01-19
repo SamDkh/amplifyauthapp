2a634a0a1fe8af6c822d91b50e86263cbe98111f
Samy Dakhlaoui <samy.dakhlaoui@rentacar.fr>
2020-01-19 18:47:28 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+import { AmplifyService } from "aws-amplify-angular";
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "amplify-app";
+  constructor(public amplify: AmplifyService) {
+    amplify
+      .auth()
+      .currentAuthenticatedUser()
+      .then(console.log);
+  }
+}
